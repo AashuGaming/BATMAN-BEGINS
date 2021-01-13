865c26c85ce5ae3982e6beb94a3b816aaf5f6cb5
AashuGaming <77149352+AashuGaming@users.noreply.github.com>
2021-01-13 17:53:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,161 @@
+var drop1, drop2, drop3, drop4, drop5, drop6, drop7, drop8, drop9, drop10, drop11, drop12, drop13, drop14, drop15, drop16, drop17, drop18, drop19, drop20, drop21, drop22, drop23, drop24, drop25, drop26, drop27, drop28, drop29, drop30, drop31, drop32, drop33, drop34, drop35, drop36, drop37, drop38, drop39, drop40, drop41, drop42, drop43, drop44, drop45, drop46, drop47, drop48, drop49, drop50;
+
+function setup(){
+   createCanvas(700, 1600);
+
+   drop1 = createSprite(200, 200, 10, 10);
+   drop1.shapeColor = "blue";
+
+   drop2 = createSprite(200, 100, 10, 10);
+   drop2.shapeColor = "blue";
+   
+   drop3 = createSprite(200, 700, 10, 10);
+   drop3.shapeColor = "blue";
+
+   drop4 = createSprite(200, 500, 10, 10);
+   drop4.shapeColor = "blue";
+
+   drop5 = createSprite(200, 550, 10, 10);
+   drop5.shapeColor = "blue";
+
+   drop6 = createSprite(300, 200, 10, 10);
+   drop6.shapeColor = "blue";
+
+   drop7 = createSprite(300, 680, 10, 10);
+   drop7.shapeColor = "blue";
+
+   drop8 = createSprite(300, 250, 10, 10);
+   drop8.shapeColor = "blue";
+
+   drop9 = createSprite(400, 200, 10, 10);
+   drop9.shapeColor = "blue";
+
+   drop10 = createSprite(500, 350, 10, 10);
+   drop10.shapeColor = "blue";
+
+   drop11 = createSprite(100, 200, 10, 10);
+   drop11.shapeColor = "blue";
+
+   drop12 = createSprite(600, 200, 10, 10);
+   drop12.shapeColor = "blue";
+
+   drop13 = createSprite(500, 600, 10, 10);
+   drop13.shapeColor = "blue";
+
+   drop14 = createSprite(690, 200, 10, 10);
+   drop14.shapeColor = "blue";
+
+   drop15 = createSprite(580, 200, 10, 10);
+   drop15.shapeColor = "blue";
+
+   drop16 = createSprite(450, 100, 10, 10);
+   drop16.shapeColor = "blue";
+
+   drop17 = createSprite(350, 200, 10, 10);
+   drop17.shapeColor = "blue";
+
+   drop18 = createSprite(150, 500, 10, 10);
+   drop18.shapeColor = "blue";
+
+   drop19 = createSprite(150, 800, 10, 10);
+   drop19.shapeColor = "blue";
+
+   drop20 = createSprite(150, 800, 10, 10);
+   drop20.shapeColor = "blue";
+
+   drop21 = createSprite(150, 400, 10, 10);
+   drop21.shapeColor = "blue";
+
+   drop22 = createSprite(150, 300, 10, 10);
+   drop22.shapeColor = "blue";
+
+   drop23 = createSprite(150, 100, 10, 10);
+   drop23.shapeColor = "blue";
+
+   drop24 = createSprite(150, 900, 10, 10);
+   drop24.shapeColor = "blue";
+
+   drop25 = createSprite(400, 900, 10, 10);
+   drop25.shapeColor = "blue";
+
+   drop26 = createSprite(550, 900, 10, 10);
+   drop26.shapeColor = "blue";
+
+   drop27 = createSprite(500, 900, 10, 10);
+   drop27.shapeColor = "blue";
+
+   drop28 = createSprite(150, 700, 10, 10);
+   drop28.shapeColor = "blue";
+
+   drop29 = createSprite(150, 600, 10, 10);
+   drop29.shapeColor = "blue";
+
+   drop30 = createSprite(150, 200, 10, 10);
+   drop30.shapeColor = "blue";
+
+   drop31 = createSprite(600, 400, 10, 10);
+   drop31.shapeColor = "blue";
+
+   drop32 = createSprite(600, 300, 10, 10);
+   drop32.shapeColor = "blue";
+
+   drop33 = createSprite(600, 500, 10, 10);
+   drop33.shapeColor = "blue";
+
+   drop34 = createSprite(300, 400, 10, 10);
+   drop34.shapeColor = "blue";
+
+   drop35 = createSprite(300, 500, 10, 10);
+   drop35.shapeColor = "blue";
+
+   drop36 = createSprite(400, 400, 10, 10);
+   drop36.shapeColor = "blue";
+
+   drop37 = createSprite(400, 600, 10, 10);
+   drop37.shapeColor = "blue";
+
+   drop38 = createSprite(400, 700, 10, 10);
+   drop38.shapeColor = "blue";
+
+   drop39 = createSprite(500, 400, 10, 10);
+   drop39.shapeColor = "blue";
+
+   drop40 = createSprite(500, 800, 10, 10);
+   drop40.shapeColor = "blue";
+
+   drop41 = createSprite(500, 700, 10, 10);
+   drop41.shapeColor = "blue";
+
+   drop42 = createSprite(100, 500, 10, 10);
+   drop42.shapeColor = "blue";
+
+   drop43 = createSprite(100, 300, 10, 10);
+   drop43.shapeColor = "blue";
+
+   drop44 = createSprite(100, 100, 10, 10);
+   drop44.shapeColor = "blue";
+
+   drop45 = createSprite(100, 700, 10, 10);
+   drop45.shapeColor = "blue";
+
+   drop46 = createSprite(700, 500, 10, 10);
+   drop46.shapeColor = "blue";
+
+   drop47 = createSprite(400, 360, 10, 10);
+   drop47.shapeColor = "blue";
+
+   drop48 = createSprite(100, 580, 10, 10);
+   drop48.shapeColor = "blue";
+
+   drop49 = createSprite(500, 750, 10, 10);
+   drop49.shapeColor = "blue";
+
+   drop50 = createSprite(300, 750, 10, 10);
+   drop50.shapeColor = "blue";
+}
+
+function draw(){
+    background("black");
+    drawSprites();
+}   
+
